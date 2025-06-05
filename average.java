e0e797cb605a812b19c20485666aa4fc76cacbd3
Likhitha-12662 <rlikhitha227@gmail.com>
2025-06-05 15:26:59 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,10 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int n1 = 10, n2 = 20, n3 = 30;
+        int avg = (n1 + n2 + n3) / 3;
+        System.out.println("Average: " + avg);
+    }
+}
+
