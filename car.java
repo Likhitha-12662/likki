f4a867437715bef9799bf957dd2bbd5ef8a63a65
Likhitha-12662 <rlikhitha227@gmail.com>
2025-06-05 15:26:25 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,20 @@
+class Main{
+public static void main(String[]args){
+Car c = new Car();
+c.brand = " maruthi " ;
+c.color = " red " ;
+c.price = 120000 ;
+System.out.println(c.brand) ; 
+System.out.println(c.color) ;
+System.out.println(c.price) ;
+c.accelerate() ;
+}
+}
+class Car{
+String brand ; 
+String color ;
+int price ;
+void accelerate() {
+System.out.println(" A car accelerates ") ;
+}
+}
