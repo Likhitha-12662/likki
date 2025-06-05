bee969672e6ffcf2454f0683a4799e45ee97db87
Likhitha-12662 <rlikhitha227@gmail.com>
2025-06-05 15:27:51 +0530

    Add files via upload

---
+++ byte.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 129;
+        byte t = 120;
+        System.out.println("A: " + a);
+        System.out.println("T: " + t);
+    }
+}
